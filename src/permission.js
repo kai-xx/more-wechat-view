b05750fb2bc7e96bfe3f83a733d4bbff7af47900
carter <zhangkaifunny@gmail.com>
2018-09-20 18:56:45 +0800

    update

--- src/permission.js
+++ src/permission.js
@@ -50,7 +50,7 @@ router.beforeEach((to, from, next) => {
   } else {
     /* has no token*/
 
-    if (whiteList.indexOf(to.path) !== -1 || (/^\/news-detail\/[0-9]{1,}$/).test('/news-detail/30')) { // 在免登录白名单，直接进入
+    if (whiteList.indexOf(to.path) !== -1 || (/^\/news-detail\/[0-9]{1,}$/).test(to.path)) { // 在免登录白名单，直接进入
       next()
     } else {
       next('/login') // 否则全部重定向到登录页
